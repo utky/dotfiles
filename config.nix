a7b368a6a9f31fbe97025d1a89b2ae77b6a78818
utky <ilyaletre@gmail.com>
2015-08-19 01:34:53 +0900

    enable personal haskell environment

--- config.nix
+++ config.nix
@@ -1,21 +1,14 @@
 {
-  packageOverrides = pkgs_: with pkgs_; {  # pkgs_ is the original set of packages
-    all = with pkgs; buildEnv {  # pkgs is your overriden set of packages itself
-      name = "all";
-      paths = [
-        # haskellngPackage.cabal-install
-        # haskellngPackage.cabal2nix
-        # cacert-20140715
-        # haskellngPackage.ghc
-        # haskell-hakyll-4.6.6.0
-        # haskellngPackage.haskell-hlint-1.9.21
-        # haskellngPackage.hdevtools-0.1.0.8
-        # haskellngPackage.husk-scheme
-        # nix-1.8
-        # nix-repl-1.8-f924081
-        # nodejs-0.12.0
-        # sbt-0.13.7
-      ];
-    };
+  packageOverrides = super: let self = super.pkgs; in
+  {
+    ilya-haskell = self.haskell.packages.ghc7101.ghcWithPackages
+                     (haskellPackages: with haskellPackages; [
+                       cabal-install
+                       cabal2nix
+                       hlint
+                       hakyll
+                       hdevtools
+                       stack
+                     ]);
   };
 }
