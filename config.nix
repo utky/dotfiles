7f83538fedebeaa3deba6753bf9625fe54a51eff
utky <ilyaletre@gmail.com>
2015-08-20 08:14:27 +0900

    ready for latest version ghc-mod

--- config.nix
+++ config.nix
@@ -1,13 +1,14 @@
 {
   packageOverrides = super: let self = super.pkgs; in
   {
-    ilya-haskell = self.haskell.packages.ghc7101.ghcWithPackages
+    ilya-haskell = self.haskell.packages.ghc7102.ghcWithPackages
                      (haskellPackages: with haskellPackages; [
                        cabal-install
                        cabal2nix
                        hlint
                        hakyll
                        hdevtools
+                       ghc-mod
                        stack
                      ]);
   };
