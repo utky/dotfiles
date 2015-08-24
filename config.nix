0ede0ce4a31ec03ed39211e1358e496d33eef86c
utky <ilyaletre@gmail.com>
2015-08-25 07:43:03 +0900

    Add various development tools as standard installation.

--- config.nix
+++ config.nix
@@ -10,6 +10,42 @@
                        hdevtools
                        ghc-mod
                        stack
+                       elm-compiler
                      ]);
+
+    ilya-jvm = with self; buildEnv {
+      name = "ilya-jvm";
+      paths = [
+        sbt
+        leiningen
+        apache-maven
+      ];
+    };
+
+    ilya-python = with self; buildEnv {
+      name = "ilya-python";
+      paths = [
+        python
+        pythonPackages.pip
+        pythonPackages.virtualenv
+        pythonPackages.virtualenvwrapper
+      ];
+    };
+
+    ilya-node = with self; buildEnv {
+      name = "ilya-node";
+      paths = [
+        nodejs
+      ];
+    };
+
+    ilya-ocaml = with self; buildEnv {
+      name = "ilya-ocaml";
+      paths = [
+        ocaml
+        coq
+      ];
+    };
+
   };
 }
