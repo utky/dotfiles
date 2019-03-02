54eeb4e10e302040e1aaee38692770c57e109666
utky <ilyaletre@gmail.com>
2019-03-03 00:19:30 +0900

    add python statistics environment

--- nix/config.nix
+++ nix/config.nix
@@ -1,20 +1,38 @@
 {
-  packageOverrides = super: let self = super.pkgs; in
-  {
-    ilya = with self; buildEnv {
-      name = "ilya";
-      paths = [
-        sqlite
-        gdb
-        global
-        plantuml
-        stack
-        elmPackages.elm
-        openjdk
-        leiningen
-        go
-        chicken
+  packageOverrides = super:
+    let
+      self = super.pkgs;
+      python-stats-packages = python-packages: with python-packages; [
+        pandas
+        scipy
+        jupyter
+        matplotlib
       ];
-    };
-  };
+      my-python = super.pkgs.python3.withPackages python-stats-packages;
+    in
+      {
+        ilya = with self; buildEnv {
+          name = "ilya";
+          paths = [
+            nix-repl
+            sqlite
+            gdb
+            global
+            plantuml
+            stack
+            elmPackages.elm
+            openjdk
+            leiningen
+            go
+            chicken
+            gitAndTools.hub
+          ];
+        };
+        ilya-python = with self; buildEnv {
+          name = "ilya-python";
+          paths = [
+            my-python
+          ];
+        };
+      };
 }
